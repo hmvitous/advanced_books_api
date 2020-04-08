76abaa9fd74172a5276ab5a16637c327f0e0ce7f
Hunter <hmvitous@yahoo.com>
2020-04-08 16:55:46 +0200

    Formats code

--- database/models/book.js
+++ database/models/book.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Book = sequelize.define('Book', {
-    title: DataTypes.STRING
-  }, {});
-  Book.associate = function(models) {
+  const Book = sequelize.define(
+    "Book",
+    {
+      title: DataTypes.STRING,
+    },
+    {}
+  );
+  Book.associate = function (models) {
     // associations can be defined here
   };
   return Book;
