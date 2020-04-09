8e75daabdc7c867d21d281fd6d5f2b9838a7a995
Hunter <hmvitous@yahoo.com>
2020-04-09 10:56:20 +0200

    Creates tests using factory girl

--- test/booksEndpoint.spec.js
+++ test/booksEndpoint.spec.js
@@ -2,6 +2,7 @@ const app = require("../app");
 const supertest = require("supertest");
 const expect = require("chai").expect;
 const jsonResponse = require("./jsonResponse");
+const {factory, Models} = require('../test_helpers')
 
 let server, request, response;
 
@@ -13,6 +14,17 @@ before((done) => {
 after((done) => {
   server.close(done);
 });
+
+beforeEach(async() => {
+  await factory.createMany('Book', 2, [
+    { id: 100, title: 'Learn NodeJS with Thomas' },
+    { id: 900, title: 'Learn NodeJS with Thomas - The Sequel' }
+  ])
+})
+
+afterEach (() => {
+  factory.cleanUp()
+})
 
 describe("GET /api/v1/books", () => {
   before(async () => {
@@ -35,11 +47,14 @@ describe("GET /api/v1/books", () => {
 });
 
 describe('GET /api/v1/books/:id', () => {
-  before(async () => {
-    response = await request.get('/api/v1/books/1')
-  })
 
-  it('responds with a single book', () => {
-    expect(response.body.book.id).to.equal(1)
+  it('responds with a single book - id', async () => {
+    response = await request.get('/api/v1/books/100')
+    expect(response.body.book.id).to.equal(100)
+  });
+
+  it('responds with a single book - title', async () => {
+    response = await request.get('/api/v1/books/900')
+    expect(response.body.book.title).to.equal('Learn NodeJS with Thomas - The Sequel')
   });
 });
